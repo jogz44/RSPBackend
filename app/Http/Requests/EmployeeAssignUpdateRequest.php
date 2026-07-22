9bf26fa605915a5f627c5d7790dd5de62ddcab04
Taskspeed <taskspeed2002@gmail.com>
2026-07-22 14:53:41 +0800

    added: request for assign and re-asssign

---
+++ app/Http/Requests/EmployeeAssignUpdateRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EmployeeAssignUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+          
+           'office' => 'nullable|string',
+           'office2' => 'nullable|string',
+           'group' => 'nullable|string',
+            'division' => 'nullable|string',
+            'section' => 'nullable|string',
+            'unit' => 'nullable|string',
+        ];
+    }
+}
---
+++ app/Http/Requests/EmployeeReAssignUpdateRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EmployeeReAssignUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //     return [
+        'office' => 'nullable|string',
+        'office2' => 'nullable|string',
+        'group' => 'nullable|string',
+        'division' => 'nullable|string',
+        'section' => 'nullable|string',
+        'unit' => 'nullable|string',
+        're_assign_date' => 'nullable|date',
+        'active' => 'required|boolean',
+        ];
+    }
+}
