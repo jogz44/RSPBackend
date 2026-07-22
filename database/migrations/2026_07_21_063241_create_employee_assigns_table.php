910ad9ed01cf9ebaf4d70cc3afd064b8f4845741
Taskspeed <taskspeed2002@gmail.com>
2026-07-22 14:54:36 +0800

    added:  table re-assign and libOffice and assign

---
+++ database/migrations/2026_07_21_063241_create_employee_assigns_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employee_assigns', function (Blueprint $table) {
+            $table->id();
+            $table->string('control_no')->nullable();
+            $table->string('office')->nullable();
+            $table->string('office2')->nullable();
+            $table->string('group')->nullable();
+            $table->string('division')->nullable();
+            $table->string('section')->nullable();
+            $table->string('unit')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee_assigns');
+    }
+};
